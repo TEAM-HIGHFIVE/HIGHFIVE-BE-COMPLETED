6a96901ca87b6b48ed85331e582eacc173bf7e79
koungq <guu119@naver.com>
2025-05-28 17:38:12 +0900

    fix: cors

---
+++ board-service/src/test/java/advancedweb/project/boardservice/ui/controller/PostControllerTest.java
@@ -0,0 +1,28 @@
+package advancedweb.project.boardservice.ui.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostControllerTest {
+
+    @Test
+    void 모든_게시물_조회() {
+    }
+
+    @Test
+    void 자주_열람하는_게시물_목록_조회() {
+    }
+
+    @Test
+    void 게시물_상세_조회() {
+    }
+
+    @Test
+    void 게시물_작성() {
+    }
+
+    @Test
+    void 게시물_삭제() {
+    }
+}
