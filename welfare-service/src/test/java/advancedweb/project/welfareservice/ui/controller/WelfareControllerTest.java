8f16d7aa35dde079a979f61e3c91451dbb79ddc5
koungq <guu119@naver.com>
2025-05-28 17:36:19 +0900

    fix: cors

---
+++ welfare-service/src/test/java/advancedweb/project/welfareservice/ui/controller/WelfareControllerTest.java
@@ -0,0 +1,24 @@
+package advancedweb.project.welfareservice.ui.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WelfareControllerTest {
+
+    @Test
+    void 복지_서비스_검색() {
+    }
+
+    @Test
+    void 추천받은_복지_서비스_조회() {
+    }
+
+    @Test
+    void 원본_파일_다운로드() {
+    }
+
+    @Test
+    void 최근_조회한_복지리스트_조회() {
+    }
+}
