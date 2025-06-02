2f07d5c965bed9fa1753808f53a7693125f01f11
koungq <guu119@naver.com>
2025-06-02 18:27:17 +0900

    chore: enum return

--- welfare-service/src/main/java/advancedweb/project/welfareservice/application/dto/response/WelfareDetailRes.java
+++ welfare-service/src/main/java/advancedweb/project/welfareservice/application/dto/response/WelfareDetailRes.java
@@ -6,11 +6,12 @@ import advancedweb.project.welfareservice.domain.entity.enums.Target;
 
 import java.time.LocalDateTime;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public record WelfareDetailRes(
         String title,       // 제목
         LocalDateTime updatedAt,    // 갱신일
-        Set<Area> areas,    // 지역 태그
+        Set<String> areas,    // 지역 태그
         Set<Target> targets,    // 지원 대상 태그
         String criteria,    // 선정 기준
         String content,     // 서비스 내용
@@ -23,7 +24,9 @@ public record WelfareDetailRes(
         return new WelfareDetailRes(
                 welfare.getSummary().getName(),
                 LocalDateTime.now(),
-                welfare.getSummary().getAreas(),
+                welfare.getSummary().getAreas().stream()
+                        .map(Area::getDesc)
+                        .collect(Collectors.toSet()),
                 welfare.getSummary().getTargets(),
                 welfare.getDetail().getCriteria(),
                 welfare.getDetail().getContent(),
